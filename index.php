10927fa7104be122cb5f254aa6508ab0c37ff8c6
MascarBone <simone.bonelli@gmail.com>
2021-10-15 15:55:55 +0200

    Foreach to print every album in the array

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="css/style.css">
     <title>PHP AJAX DISCHI</title>
 </head>
 <body>
@@ -20,17 +20,22 @@
     
     
 <div class="wrapper">
+    <?php foreach($db as $album) { ?>
+    
     <div class="album">
         <div class="album-img">
-            <img style="width:200px" src="<?php echo $db[0]['poster']; ?>" alt="cover album">
+            <img style="width:200px" src="<?php echo $album['poster']; ?>" alt="cover album">
         </div>
         <div class="album-text">
-            <h2><?php echo $db[0]['title']; ?></h2>
-            <p><?php echo $db[0]['author']; ?></p>
-            <p><?php echo $db[0]['genre']; ?></p>
-            <p><?php echo $db[0]['year']; ?></p>
+            <h2><?php echo $album['title']; ?></h2>
+            <p><?php echo $album['author']; ?></p>
+            <p><?php echo $album['genre']; ?></p>
+            <p><?php echo $album['year']; ?></p>
         </div>
     </div>
+
+    <?php } ?>
+    
 </div>
 
 </body>
