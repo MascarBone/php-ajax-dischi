c7b22dbe52933e765db88c38987d2fdc029be10f
MascarBone <simone.bonelli@gmail.com>
2021-10-15 18:03:13 +0200

    Refactoring

--- index.php
+++ index.php
@@ -8,14 +8,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Gluten:wght@100;200;400;600&display=swap" rel="stylesheet"> 
     <title>PHP AJAX DISCHI</title>
 </head>
 <body>
-<?php include './database.php'; ?>
+<?php include __DIR__ . './database.php'; ?>
 
-<?php include './partials/header.php'; ?>
+<?php include __DIR__ . './partials/header.php'; ?>
 
-<?php include './partials/main.php'; ?>
+<?php include __DIR__ . './partials/main.php'; ?>
 
 
 </body>
